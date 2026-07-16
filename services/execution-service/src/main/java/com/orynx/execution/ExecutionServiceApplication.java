f9425c54d3ce081638084185a491240c643bba03
Yashu Youwaraj <amanyasu@gmail.com>
2026-07-16 13:41:11 +0530

    feat(execution): bootstrap Execution Service microservice

---
+++ services/execution-service/src/main/java/com/orynx/execution/ExecutionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orynx.execution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutionServiceApplication.class, args);
+	}
+
+}
